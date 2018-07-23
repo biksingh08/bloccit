ae4d2a943c5fc978063d636c8e850dcb56d8c64f
Bikramjit Singh <bigsingh@ucdavis.edu>
2018-07-23 10:39:19 -0700

    assignment answers to intro to express

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,8 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
+
 
 describe("routes : static", () => {
 
@@ -12,6 +14,8 @@ describe("routes : static", () => {
 
 //#3
       request.get(base, (err, res, body) => {
+        console.log(res.body);
+        console.log(res.statusCode);
         expect(res.statusCode).toBe(200);
 
 //#4
@@ -19,5 +23,19 @@ describe("routes : static", () => {
       });
     });
 
+  });
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200", (done) => {
+
+      request.get(marco, (err, res, body) => {
+        console.log(res.body);
+        expect(res.body).toBe("polo");
+        console.log(res.statusCode);
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
   });
 });
