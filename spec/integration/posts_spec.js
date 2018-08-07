b6821c7f02120c2f5eaea1f440a89a33bb15ab93
Bikramjit Singh <bigsingh@ucdavis.edu>
2018-08-07 09:26:22 -0700

    checkpoint validations answers

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -53,33 +53,63 @@ describe("routes : posts", () => {
 
   describe("POST /topics/:topicId/posts/create", () => {
 
-  it("should create a new post and redirect", (done) => {
-     const options = {
-       url: `${base}/${this.topic.id}/posts/create`,
-       form: {
-         title: "Watching snow melt",
-         body: "Without a doubt my favoriting things to do besides watching paint dry!"
-       }
-     };
-     request.post(options,
-       (err, res, body) => {
+    it("should create a new post and redirect", (done) => {
+       const options = {
+         url: `${base}/${this.topic.id}/posts/create`,
+         form: {
+           title: "Watching snow melt",
+           body: "Without a doubt my favoriting things to do besides watching paint dry!"
+         }
+       };
+       request.post(options,
+         (err, res, body) => {
 
-         Post.findOne({where: {title: "Watching snow melt"}})
-         .then((post) => {
-           expect(post).not.toBeNull();
-           expect(post.title).toBe("Watching snow melt");
-           expect(post.body).toBe("Without a doubt my favoriting things to do besides watching paint dry!");
-           expect(post.topicId).not.toBeNull();
-           done();
-         })
-         .catch((err) => {
-           console.log(err);
-           done();
-         });
-       }
-     );
-   });
-  });
+           Post.findOne({where: {title: "Watching snow melt"}})
+           .then((post) => {
+             expect(post).not.toBeNull();
+             expect(post.title).toBe("Watching snow melt");
+             expect(post.body).toBe("Without a doubt my favoriting things to do besides watching paint dry!");
+             expect(post.topicId).not.toBeNull();
+             done();
+           })
+           .catch((err) => {
+             console.log(err);
+             done();
+           });
+         }
+       );
+     });
+
+     it("should not create a new post that fails validations", (done) => {/* spec implementation */});
+
+      it("should not create a new post that fails validations", (done) => {
+        const options = {
+          url: `${base}/${this.topic.id}/posts/create`,
+          form: {
+
+    //#1
+            title: "a",
+            body: "b"
+          }
+        };
+
+        request.post(options,
+          (err, res, body) => {
+
+    //#2
+            Post.findOne({where: {title: "a"}})
+            .then((post) => {
+                expect(post).toBeNull();
+                done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          }
+        );
+      });
+      });
 
   describe("GET /topics/:topicId/posts/:id", () => {
 
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const validation = require("./validation");
 const postController = require("../controllers/postController");
 
 router.get("/topics/:topicId/posts/new", postController.new);
@@ -8,9 +9,9 @@ router.get("/topics/:topicId/posts/:id", postController.show);
 
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 
-router.post("/topics/:topicId/posts/create", postController.create);
+router.post("/topics/:topicId/posts/create", validation.validatePosts, postController.create);
 
-router.post("/topics/:topicId/posts/:id/update", postController.update);
+router.post("/topics/:topicId/posts/:id/update", validation.validatePosts, postController.update);
 
 router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
 
